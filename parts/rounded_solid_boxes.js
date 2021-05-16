3b1c59fd3b30b4d342cfe62802914fd4cc7a0edf
raydeleu <38007983+raydeleu@users.noreply.github.com>
2021-05-17 00:26:43 +0200

    Update rounded_solid_boxes.js

--- parts/rounded_solid_boxes.js
+++ parts/rounded_solid_boxes.js
@@ -15,30 +15,29 @@ function RoundAll(shape,fillet)
     return grown_version
 }
 
+let t = 0.5
+
 // only plan-view rounded through fillets in sketch
-let box0 = Translate([-20,0,0],Extrude(filletsquare(10,20,5),[0,0,20]));
+let box0 = Translate([-20,0,0],Extrude(filletsquare(10,20,3),[0,0,20]));
+let boxi = Translate([0,0,0],Offset(box0,-2*t))
+let boxir = Offset(boxi,t)
+let boxh = Difference(box0,[boxir])
+let boxh2 = Difference(boxh,[Translate([-20,10,15],Box(50,50,50))]) 
+
 
 // all edges rounded individually
-let box1 = Extrude(filletsquare(10,20,5),[0,0,20]);
+let box1 = Extrude(filletsquare(10, 20, 5), [0,0,20]);
 let rounded_box = FilletEdges(box1, 2, [3,6,12,15,17]);
 let double_rounded_box = FilletEdges(rounded_box, 1, [11,12,10,13,6,2]);
 
-// NOTE:
-// In the example above there is a warning of encountering a NULL face, 
-// which may be caused by the fact that the two fillets in filletsquare
-// are in fact touching, completely removing the straight part between
-// the fillets. 
-
 // using offsets
-let box2 = Translate([20,0,0], box1)
+let box2 = Translate([40,0,0], box0, true)
 let shrunk_box= Offset(box2,-2)
 let grown_box = Offset(shrunk_box,2)
 
 // using a function 
-let box3 = Translate([40,0,0], box1)
-let rounded_box3 = RoundAll(box3,3)
+let box3 = Translate([60,0,0], box0, true)
+let rounded_box3 = RoundAll(box3,2)
 
 // using a function on a rectangular, unfilleted box
 let box4 = Translate([60,0,0],RoundAll(Box(10,20,20),5))
-
-
